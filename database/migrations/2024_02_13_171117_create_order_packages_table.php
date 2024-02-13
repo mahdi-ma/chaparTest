56c4c394f22e3c38476c07a492b394048f64cc46
MahdiMa <mahi.maghsoodi@hotmail.com>
2024-02-13 21:25:33 +0330

    feat(Order Package): add order package migration

--- database/migrations/2024_02_13_171117_create_order_packages_table.php
+++ database/migrations/2024_02_13_171117_create_order_packages_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('order_packages', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Order::class, 'order_id')
+                ->constrained('orders')
+                ->cascadeOnUpdate();
+            $table->string('barcode');
+            $table->double('weight', 8, 2);
             $table->timestamps();
         });
     }
