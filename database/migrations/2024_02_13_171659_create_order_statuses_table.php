a38e214063fe9bb7628bc3a4a788784f2b534e1c
MahdiMa <mahi.maghsoodi@hotmail.com>
2024-02-13 21:24:05 +0330

    feat(Status): add order migration and service and seeder

--- database/migrations/2024_02_13_171659_create_order_statuses_table.php
+++ database/migrations/2024_02_13_171659_create_order_statuses_table.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\Order;
+use App\Models\Status;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('order_statuses', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Order::class, 'order_id')
+                ->constrained('orders')
+                ->cascadeOnUpdate();
+            $table->foreignIdFor(Status::class, 'status_id')
+                ->constrained('statuses')
+                ->cascadeOnUpdate();
+            $table->foreignIdFor(User::class, 'created_by')
+                ->nullable()
+                ->constrained('users')
+                ->cascadeOnUpdate();
             $table->timestamps();
         });
     }
--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -2,16 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Status extends Seeder
+class StatusSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $statuses = [
+            [
+                'id' => 1,
+                'name' => 'ثبت سفارش',
+            ],
+            [
+                'id' => 1,
+                'name' => 'جمع آوری',
+            ],
+            [
+                'id' => 1,
+                'name' => 'پردازش در هاب',
+            ],
+            [
+                'id' => 1,
+                'name' => 'توزیع',
+            ],
+            [
+                'id' => 1,
+                'name' => 'تحویل داده شده',
+            ],
+        ];
+        Status::query()->upsert($statuses, 'id');
+
     }
 }
